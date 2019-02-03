874acf8eaaa40946a979a9916251e3c592885235
tilz0R <tilen.majerle@gmail.com>
2019-02-03 14:17:13 +0100

    Expand to git update

--- docs/src/_application_note.h
+++ docs/src/_application_note.h
@@ -16,20 +16,35 @@
  *      - This repository uses `GPS_NMEA_Parser` repository as `submodule`
  *	    - Repository is used to evaluate library using prepared examples
  *
- * \subsection      subsect_clone_res Clone resources repository with examples
+ * \subsection      subsect_clone_res Resources repository with examples
  *
  * Easiest way to test the library is to clone resources repository.
+ *
+ * \par             First-time clone
  *
  *  - Download and install `git` if not already
  *  - Open console and navigate to path in the system to clone repository to. Use command `cd your_path`
  *  - Run `git clone --recurse-submodules https://github.com/MaJerle/GPS_NMEA_Parser_res` command to clone repository including submodules
  *  - Navigate to `examples` directory and run favourite example
  *
- * \subsection      subsect_clone_lib Clone library only
+ * \par             Already cloned, update to latest version
+ *
+ *  - Open console and navigate to path in the system where your resources repository is. Use command `cd your_path`
+ *  - Run `git pull origin master --recurse-submodules` command to pull latest changes and to fetch latest changes from submodules
+ *  - Run `git submodule foreach git pull origin master` to update & merge all submodules
+ *
+ * \subsection      subsect_clone_lib Library only
  *
  * If you are already familiar with library and you wish to include it in existing project, easiest way to do so is to clone library repository only.
+ *
+ * \par             First-time clone
  *
  *  - Download and install `git` if not already
  *  - Open console and navigate to path in the system to clone repository to. Use command `cd your_path`
  *  - Run `git clone --recurse-submodules https://github.com/MaJerle/GPS_NMEA_Parser` command to clone repository
+ * 
+ * \par             Already cloned, update to latest version
+ *
+ *  - Open console and navigate to path in the system where your repository is. Use command `cd your_path`
+ *  - Run `git pull origin master --recurse-submodules` to update & merge latest repository changes
  */
